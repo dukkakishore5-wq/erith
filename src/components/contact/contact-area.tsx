591db1ef67f71d7e2de38d684ed1a4f405829938
Benny453 <bennypaul569@gmail.com>
2025-11-01 20:41:23 +0530

    fix all errors

--- src/components/contact/contact-area.tsx
+++ src/components/contact/contact-area.tsx
@@ -3,8 +3,8 @@ import ContactForm from "../form/contact-form";
 // import { HomeSvg } from "../svg";
 
 type IProps = {
-  title: string;
-  subtitle: string;
+  title?: string;
+  subtitle?: string;
   admission?: boolean;
 };
 
@@ -22,14 +22,9 @@ export default function ContactArea({ title, subtitle, admission }: IProps) {
             <div className="col-sm-12">
               <div className="tp-breadcrumb__content">
                 <div className="tp-breadcrumb__list inner-after">
-                  {/* <span className="white">
-                  <Link href="/">
-                    <HomeSvg/>
-                  </Link>
-                </span> */}
                   {admission && <span className="white">Admission</span>}
-                  {title && <span className="white"></span>}
-                  {subtitle && <span className="white"></span>}
+                  {title && <span className="white">{title}</span>}
+                  {subtitle && <span className="white">{subtitle}</span>}
                   <h3 className="tp-breadcrumb__title color">Get In Touch</h3>
                   <span className="white">
                     We Are Here To Answer Any Questions You May Have
@@ -40,38 +35,9 @@ export default function ContactArea({ title, subtitle, admission }: IProps) {
           </div>
         </div>
       </section>
+
       <section className="tp-contact-area tp-contact-p fix p-relative pt-10 pb-20">
-        {/* <div className="tp-contact-bg" style={{ backgroundImage: "url(/assets/img/live/erith-2.jpeg)" }}></div> */}
-        {/* <div className="tp-contact-shape">
-        <span>
-          <svg
-            width="1920"
-            height="559"
-            viewBox="0 0 1920 559"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1958.7 6.55286C1332.03 310.106 2369.35 119.238 2232.58 220.873C2018.48 379.976 692.5 607.75 254.5 538.145C-27.1058 493.393 1387 130.595 -280 395.595"
-              stroke="url(#paint0_linear_2756_1168)"
-              strokeWidth="14"
-            />
-            <defs>
-              <linearGradient
-                id="paint0_linear_2756_1168"
-                x1="92.1912"
-                y1="171.542"
-                x2="1827.4"
-                y2="294.717"
-                gradientUnits="userSpaceOnUse"
-              ></linearGradient>
-            </defs>
-          </svg>
-        </span>
-      </div> */}
-        <div className="tp-contact-shape-2">
-          {/* <img src="/assets/img/live/contact-shape-2.svg" alt="shape" /> */}
-        </div>
+        <div className="tp-contact-shape-2"></div>
         <div className="container">
           <div
             className="tp-about-wrap mb-60 wow fadeInLeft"
